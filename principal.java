89d6f9f7a34ac7e7262b4eb6e8e854c637e1ca14
Alejandra1401 <alejandra.reyes.dias17@gmail.com>
2025-02-17 07:10:12 -0500

    menu en principal

---
+++ principal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class principal
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int opt =0, d = 0;
+        System.out.println("Ingrese el punto que desea visualizar");
+        opt =sc.nextInt();
+        System.out.println("Ingrese la dimesion de la matriz");
+        d = sc.nextInt();
+        switch (opt) 
+        {
+            case 1:
+                break;
+        
+            default:
+                break;
+        }
+        
+    }
+}
